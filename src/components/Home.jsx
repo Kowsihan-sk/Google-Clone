b9399e35589243501e1940c3cf96ed1ada08c118
kowsihan-sk <kowsihan2sk@gmail.com>
2021-06-01 17:53:12 +0530

    initial commit

---
+++ src/components/Home.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Home = () => {
+    return (
+        <div className="home">
+            <div className="home_container">
+                <div className="home_logo">
+                    <img src="/images/googleLogo.png" alt="Google Logo" />
+                    <form action="">
+                        <input type="text" name="" id="" />
+                        <button type="submit">Search</button>
+                    </form>
+                </div>
+            </div>
+        </div >
+    )
+}
+
+export default Home
